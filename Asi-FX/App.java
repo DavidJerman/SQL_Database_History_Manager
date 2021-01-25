9cb82610b96f583a0a37e3ac6f949763007948e3
david02 <davidjerman3@gmail.com>
2021-01-25 16:15:02 +0100

    Dodal ogrodje za grafično SQL aplikacijo.

---
+++ Asi-FX/App.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * GUI/Front-end of the SQL application
+ *
+ * @author David Jerman
+ * @version 2021.01.25
+ * @since 2021.01.25
+ */
+public class App extends Application {
+
+    /**
+     * Main method
+     *
+     * @param args Accepts no arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * The start function of the application
+     *
+     * @param primaryStage Primary Stage
+     */
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
