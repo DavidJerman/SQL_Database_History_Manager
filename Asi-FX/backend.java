8e3c8a0a256408f5fa67bc5abeb423d2ae07bfcc
RokMv <rokmlinar.p@gmail.com>
2021-01-25 17:12:44 +0100

    Dodal nekaj privatnih funkcij v backend.java

--- Asi-FX/backend.java
+++ Asi-FX/backend.java
@@ -231,25 +231,30 @@ public class backend {
 
 
     /**
-     Opis
+     Funkcija IP pretvori v ime preko avtorjev ustvarjenih poimenovanih tabel
 
-     @param
-     @return Vracanje
-     @thorws Exception
+     @param IP
+     @return name
      **/
-    private String crossReferenceIP(){
+    private String crossReferenceIP(String IP){
 
         return null;
     }
 
     /**
-     Opis
+     Ustvari hashmap iz IP-jev in avtorjev ustvarjenih poimenovanih tabel
 
-     @param
-     @return Vracanje
+     @return hashmap
      @thorws Exception
      **/
     private void setupNameIP_map(){
+
+    }
+
+    /**
+     Funkcija preveri če je povezava vspostavljena in jo, če ni vspostavi
+     **/
+    private void checkConnection(){
 
     }
 
