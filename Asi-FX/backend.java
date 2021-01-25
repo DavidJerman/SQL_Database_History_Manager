977532998792400682a8990c4d5d036756f70d96
RokMv <rokmlinar.p@gmail.com>
2021-01-25 14:47:20 +0100

    Spisal sem ogrodje za backend.java

---
+++ Asi-FX/backend.java
@@ -0,0 +1,226 @@
+import java.sql.*;
+
+/**
+ * Communication with the SQL database log
+ *
+@author - Rok Mlinar Vahtar, Gašper Suhadolnik
+@version - 25.1.2021
+@since - 25.1.2021
+**/
+public class backend {
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getTableInfo(String tableName){
+
+        return null;
+    }
+
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhoInserted(String tableName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhenInserted(String dataValue, String userName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhatInserted(String userName){
+
+        return null;
+    }
+
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhoSelected(String tableName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhenSelected(String dataValue, String userName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhatSelected(String userName){
+
+        return null;
+    }
+
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhoUpdated(String tableName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhenUpdated(String dataValue, String userName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhatUpdated(String userName){
+
+        return null;
+    }
+
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhoDeleted(String tableName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhenDeleted(String dataValue, String userName){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getWhatDeleted(String userName){
+
+        return null;
+    }
+
+
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getAllUsers(){
+
+        return null;
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    String[] getAllTables(){
+
+        return null;
+    }
+
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    void setCurrentDatabase(String databaseName){
+
+
+    }
+
+    /**
+     Opis
+
+     @param
+     @return Vracanje
+     @thorws Exception
+     **/
+    ResultSet getCustomQuery(String sqlSelectStatement){
+
+        return null;
+    }
+
+
+
+
+}
