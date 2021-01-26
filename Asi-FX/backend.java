5dc62f51dec068a0cdf7bc2beb923088684a18f8
RokMv <rokmlinar.p@gmail.com>
2021-01-27 00:28:33 +0100

    Delno spisal metodo setupNameIP_map()

--- Asi-FX/backend.java
+++ Asi-FX/backend.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -12,6 +13,7 @@ public class backend {
 
     Connection connection; //Povazava z SQL serverjem
     String currentDatabase; //Ime baze na kateri trenutno izvajamo operacije
+    HashMap<String,String> nameAndIP_map; //Map imen in ip naslovov
 
     /**
      Opis
@@ -227,10 +229,72 @@ public class backend {
 
 
 
+
+
+    /**
+     Funkcija IP pretvori v ime preko avtorjev ustvarjenih poimenovanih tabel
+
+     @param IP
+     @return name
+     **/
+    private String crossReferenceIP(String IP){
+
+        return null;
+    }
+
+    /**
+     Ustvari hashmap iz IP-jev in avtorjev ustvarjenih poimenovanih tabel
+
+     @return hashmap
+     **/
+    public void setupNameIP_map(){
+        checkConnection();//preveri povezavo
+
+        Statement statementmt=null;
+        ResultSet resultSet=null;
+        try {
+            statementmt = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            resultSet = statementmt.executeQuery("CALL tableCreators();");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            while (resultSet.next()) {
+                String user_host = resultSet.getString(2);
+                String argument = resultSet.getString(6);
+
+
+                try {
+                    //Preveri, da se argument začne z CREATE TABLE
+                    if (argument.split(" ")[0].equals("CREATE") && argument.split(" ")[1].equals("TABLE")) {
+                        argument = argument.split(" ")[2];
+
+                        //Odstrani '' okoli imena
+                        if (argument.charAt(0) == '`' || argument.charAt(0) == '\'') argument = argument.substring(1, argument.length() - 1);
+
+                        //Dovoli le T+Priimek+Ime tabele
+                        if (!(argument.charAt(0) == 'T'))continue;
+
+                        //////////////////////////////////////////////////////////ZAKAJ JE TAKO MALO CREATE T-Tabel
+                        System.out.println(user_host + " " + argument);
+                    } else continue;
+
+                }catch (ArrayIndexOutOfBoundsException e){
+                    continue;
+                }
+            }
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
     /**
      Funkcija preveri če je povezava vspostavljena in jo, če ni, vspostavi
      **/
-    public void checkConnection(){
+    private void checkConnection(){
         try {
             if(connection == null || connection.isClosed()) {
                 try {
